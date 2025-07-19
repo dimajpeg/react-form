2bf6ac3d3eb5c00a9b42d5cc61e864009571632c
dima <>
2025-07-19 13:21:41 +0300

    Refactor NameForm component by removing comments and cleaning up code

--- src/App.js
+++ src/App.js
@@ -2,15 +2,8 @@
 import React, { useState } from 'react';
 
 function NameForm() {
-  // 1. Создаем состояние для хранения того, что введено в input.
-  //    Начальное значение - пустая строка.
   const [name, setName] = useState('');
-
-  // 2. Эта функция будет вызываться при КАЖДОМ изменении в поле ввода.
-  //    React автоматически передает в нее специальный объект 'event'.
   const handleInputChange = (event) => {
-    // У этого объекта есть свойство 'target', которое указывает на сам <input>.
-    // А у 'target' есть свойство 'value' - это и есть актуальный текст из поля.
     setName(event.target.value);
   };
 
@@ -19,12 +12,10 @@ function NameForm() {
       <h1>Привет, {name || 'Незнакомец'}!</h1>
 
       <p>Введите ваше имя:</p>
-
-      {/* 3. Связываем <input> с нашим состоянием */}
       <input
         type="text"
-        value={name}         // Значение инпута ВСЕГДА равно тому, что в 'name'
-        onChange={handleInputChange} // При любом изменении вызываем нашу функцию
+        value={name}         
+        onChange={handleInputChange} 
       />
     </>
   );
